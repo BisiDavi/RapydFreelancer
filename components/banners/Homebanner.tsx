46ef7d11d9a8e66ae4c64929bb48b57d6e064a88
BisiDavi <oludavidconnect@gmail.com>
2022-08-29 13:23:38 +0100

    homebanner updated

--- components/banners/Homebanner.tsx
+++ components/banners/Homebanner.tsx
@@ -1,21 +1,38 @@
-import TypewriterEffect from "@/components/UI/Typewriter";
 import Image from "next/image";
+
+import TypewriterEffect from "@/components/UI/Typewriter";
+import RapydLogo from "@/public/RapydLogo";
+import Button from "@/components/UI/Button";
 
 export default function Homebanner() {
   return (
-    <section className="banner justify-between flex items-center px-5  bg-gray-100 w-full">
+    <section className="banner justify-between flex items-center p-5  bg-gray-100 w-full">
       <div className="w-1/2">
         <TypewriterEffect />
         <p className="font-light text-lg -mt-4 underline text-gray-600">
           Hire top talent in minutes not weeks
         </p>
-        <p className="w-2/3 text-xl font-light mt-4">
+        <p className="w-2/3 text-xl font-light my-4">
           <span className="italic mr-1 font-bold">RapydFreelancer</span>
           is an exclusive network of the top freelance software developers,
           copywriters, product managers, designers, finance experts and data
           scientist in the world. Top companies hire RapydFreelancers for their
           projects.
         </p>
+        <div className="button-group w-72 flex items-center justify-between mt-4">
+          <Button
+            text="Hire Talent"
+            className="bg-blue-500 text-white px-4 py-1 rounded-full hover:bg-blue-800"
+          />
+          <Button
+            text="Find Work"
+            className="border border-blue-500 text-blue-500 px-4 py-1 rounded-full hover:bg-blue-800 hover:text-white"
+          />
+        </div>
+        <div className="brands mt-10">
+          <h6 className="mb-2 font-medium">Trusted and powered by leading brand</h6>
+          <RapydLogo />
+        </div>
       </div>
       <div className="w-1/2 flex mx-auto justify-center">
         <div className="w-2/3">
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,5 +1,5 @@
 import Logo from "@/components/logo";
-import Button from "@/components/button";
+import Button from "@/components/UI/Button";
 
 export default function Header() {
   return (
@@ -10,16 +10,20 @@ export default function Header() {
         content="Hire a freelancer for that your project for quick and reliable execution"
       />
       <link rel="icon" href="/favicon.ico" />
-      <header className="flex items-center shadow py-3">
+      <header className="flex w-full   items-center shadow py-3">
         <div className="container mx-auto flex items-center justify-between">
           <Logo />
-          <div className="w-44 justify-between flex">
+          <div className="w-1/3 justify-between flex">
             <Button
               text="Login"
               className="text-blue-500 font-bold rounded-md text-white hover:text-blue-800"
             />
             <Button
               text="Sign Up"
+              className="border border-blue-500 px-6 py-1.5 font-bold rounded-full text-blue-500 hover:bg-blue-800 hover:text-white"
+            />
+            <Button
+              text="Post a Job"
               className="bg-blue-500 px-6 py-1.5 font-bold rounded-full text-white hover:bg-blue-800"
             />
           </div>
