0f433529c8b2d70ac45759c750779b25a612cd0d
BisiDavi <oludavidconnect@gmail.com>
2022-08-29 21:32:25 +0100

    gallery view updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Homebanner from "@/components/banners/Homebanner";
 import InfoBanner from "@/components/banners/InfoBanner";
 import Container from "@/components/UI/Container";
 import DefaultLayout from "@/layout/DefaultLayout";
-import GalleryView from "@/Views/GalleryView";
+import GalleryView from "@/views/GalleryView";
 
 import type { NextPage } from "next";
 
