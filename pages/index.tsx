1d5e0e7ede1be95c9cc6308d2a12d91e54928a16
BisiDavi <oludavidconnect@gmail.com>
2022-08-29 21:06:19 +0100

    category gallery view added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Homebanner from "@/components/banners/Homebanner";
 import InfoBanner from "@/components/banners/InfoBanner";
 import Container from "@/components/UI/Container";
 import DefaultLayout from "@/layout/DefaultLayout";
+import CategoryGalleryView from "@/views/CategoryGalleryView";
 
 import type { NextPage } from "next";
 
@@ -11,6 +12,7 @@ const Home: NextPage = () => {
       <Container>
         <Homebanner />
         <InfoBanner />
+        <CategoryGalleryView />
       </Container>
     </DefaultLayout>
   );
