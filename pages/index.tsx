10cdb356a0acc64a8336a3584a0f569d984885ab
BisiDavi <oludavidconnect@gmail.com>
2022-08-29 21:28:41 +0100

    gallery view updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Homebanner from "@/components/banners/Homebanner";
 import InfoBanner from "@/components/banners/InfoBanner";
 import Container from "@/components/UI/Container";
 import DefaultLayout from "@/layout/DefaultLayout";
-import CategoryGalleryView from "@/views/CategoryGalleryView";
+import GalleryView from "@/views/GalleryView";
 
 import type { NextPage } from "next";
 
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
       <Container>
         <Homebanner />
         <InfoBanner />
-        <CategoryGalleryView />
+        <GalleryView />
       </Container>
     </DefaultLayout>
   );
