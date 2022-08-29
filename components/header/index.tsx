c9348cc9b91bcb3299b7b3632010e47d535c70b6
BisiDavi <oludavidconnect@gmail.com>
2022-08-29 11:45:00 +0100

    hooks added

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -10,17 +10,17 @@ export default function Header() {
         content="Hire a freelancer for that your project for quick and reliable execution"
       />
       <link rel="icon" href="/favicon.ico" />
-      <header className="flex items-center shadow py-2">
+      <header className="flex items-center shadow py-3">
         <div className="container mx-auto flex items-center justify-between">
           <Logo />
           <div className="w-44 justify-between flex">
             <Button
               text="Login"
-              className="text-blue-500 font-bold px-2 py-1 rounded-md text-white hover:bg-blue-200"
+              className="text-blue-500 font-bold rounded-md text-white hover:text-blue-800"
             />
             <Button
-              text="Signup"
-              className="bg-blue-500 px-4 py-1.5 font-bold rounded-full text-white hover:bg-blue-200"
+              text="Sign Up"
+              className="bg-blue-500 px-4 py-1.5 font-bold rounded-full text-white hover:bg-blue-800"
             />
           </div>
         </div>
