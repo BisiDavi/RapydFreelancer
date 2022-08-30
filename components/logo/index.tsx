f294c403d59d020af1d1985dc10edb6ba9e29948
BisiDavi <oludavidconnect@gmail.com>
2022-08-30 19:20:38 +0100

    logo updated

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function Logo() {
   return (
     <div className="bg-blue-500 px-4 py-2">
-      <h4 className="font-bold text-white text-2xl">
-        Rapyd <span className="italic -ml-2 text-blue-300">Freelancers</span>
-      </h4>
+      <Link href="/">
+        <a className="font-bold text-white text-2xl">
+          Rapyd <span className="italic -ml-2 text-blue-300">Freelancers</span>
+        </a>
+      </Link>
     </div>
   );
 }
