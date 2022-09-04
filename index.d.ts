1e0817ae49340d3c7b1944ee056e7164a10bb0f6
BisiDavi <oludavidconnect@gmail.com>
2022-09-04 03:25:55 +0100

    rapyd configured

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
 declare module 'nprogress'
 declare module "uuid";
+declare module "crypto-js";
---
+++ lib/rapydSignature.ts
@@ -0,0 +1,49 @@
+import CryptoJS from "crypto-js";
+
+const salt = CryptoJS.lib.WordArray.random(12); // Randomly generated for each request.
+const timestamp = (Math.floor(new Date().getTime() / 1000) - 10).toString(); // Current Unix time (seconds).
+const access_key = process.env.NEXT_PUBLIC_SANDBOX_RAPYD_ACCESS_KEY; // The access key from Client Portal.
+const secret_key = process.env.NEXT_PUBLIC_SANDBOX_RAPYD_SECRET_KEY; // Never transmit the secret key by itself.
+// Hardkeyed for this example.
+// const http_method = "get"; // get|put|post|delete - must be lowercase.
+// const data = ""; // Stringified JSON without whitespace.
+// Always empty string for GET;
+
+type httpMethodType = "get" | "post" | "delete";
+
+export default function dataRequest(
+  urlPath: string,
+  httpMethod: httpMethodType,
+  data: string
+) {
+  const getSignature = () => {
+    const to_sign =
+      httpMethod + urlPath + salt + timestamp + access_key + secret_key + data;
+    let signature = CryptoJS.enc.Hex.stringify(
+      CryptoJS.HmacSHA256(to_sign, secret_key)
+    );
+
+    signature = CryptoJS.enc.Base64.stringify(
+      CryptoJS.enc.Utf8.parse(signature)
+    );
+
+    return signature;
+  };
+  const headers = {
+    access_key,
+    signature: getSignature(),
+    salt,
+    timestamp,
+    "Content-Type": "application/json",
+  };
+  return {
+    baseURL: "https://sandboxapi.rapyd.net",
+    headers,
+    url: urlPath,
+    method: httpMethod,
+    data,
+  };
+}
+
+// You can use any HTTP request library to make the request. Example: Axios
+// const response = await axios(request);
