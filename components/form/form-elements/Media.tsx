517a90903e28b8b25c6bc2c0627f0d0ff312c649
BisiDavi <oludavidconnect@gmail.com>
2022-09-05 20:43:29 +0100

    media upload updated

--- components/form/form-elements/Media.tsx
+++ components/form/form-elements/Media.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+
+import useMediaUpload from "@/hooks/useMediaUpload";
 import type { elementType } from "@/types/form-types";
 
 export default function Media({ content }: elementType) {
+  const { uploadMedia } = useMediaUpload();
+  const [file, setFile] = useState(null);
+
+  function onClickHandler(e: any) {
+    if (e.target.files) {
+      uploadMedia(e.target.files[0]);
+    }
+  }
+
   return (
     <div className="form input border-4 border-dotted mt-4  flex items-center justify-between py-2 px-3 rounded-lg">
       <input
@@ -8,6 +20,8 @@ export default function Media({ content }: elementType) {
         placeholder={content.placeholder}
         className="rounded-lg px-10 border-2 h-8 w-1/3 mx-4"
         type="file"
+        onClick={onClickHandler}
+        accept="image/* , application/pdf"
       />
       <label htmlFor={content.name} className="text-xs w-2/3">
         {content.label}
