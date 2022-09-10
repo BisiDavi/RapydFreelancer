504edea33535938f12b8b64544d94477c6c5cdf3
BisiDavi <oludavidconnect@gmail.com>
2022-09-11 00:34:03 +0100

    form updated

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -4,15 +4,24 @@ import { profileSchema } from "@/components/form/schema/profileSchema";
 
 import profileFormContent from "@/json/profile-form.json";
 import displayFormElement from "@/lib/displayFormElement";
+import Button from "@/components/UI/Button";
 
 export default function ProfileForm() {
   const methods = useForm({
     resolver: yupResolver(profileSchema),
     mode: "all",
   });
+
+  function onSubmitHandler(data: any) {
+    console.log("data", data);
+  }
+
   return (
     <FormProvider {...methods}>
-      <form className="w-4/5 mx-auto bg-white  p-4 px-4 rounded my-4">
+      <form
+        className="w-4/5 mx-auto bg-white  p-4 px-4 rounded my-4"
+        onSubmit={methods.handleSubmit(onSubmitHandler)}
+      >
         {profileFormContent.map((formItem, index) => (
           <div key={index} className="form-group flex justify-between">
             {formItem.map((formElement) => (
@@ -22,6 +31,11 @@ export default function ProfileForm() {
             ))}
           </div>
         ))}
+        <Button
+          text="Submit"
+          type="submit"
+          className="bg-blue-500 text-white mt-4 px-4 py-1 mx-auto flex rounded hover:bg-blue-900"
+        />
       </form>
     </FormProvider>
   );
--- components/form/form-elements/Select.tsx
+++ components/form/form-elements/Select.tsx
@@ -1,6 +1,7 @@
 import { useFormContext } from "react-hook-form";
 
 import type { elementType } from "@/types/form-types";
+import countriesData from "@/json/countries.json";
 
 export default function Select({ content }: elementType) {
   const {
@@ -12,9 +13,14 @@ export default function Select({ content }: elementType) {
   const priceValue = watch("price") ? watch("price") : 0;
 
   const inputValue = content.name === "pricePeriod" ? `$${priceValue} / ` : "";
-  const labelClassName = content?.inputStyle
-    ? "font-medium my-1 text-base"
-    : "font-bold my-1 text-lg";
+  const labelClassName =
+    content?.inputStyle === "big"
+      ? "font-bold my-1 text-lg"
+      : "font-medium my-1 text-base";
+
+  const countries: { name: string; Iso2: string | any }[] = countriesData.data;
+
+  const optionsArray = content.name === "country" ? countries : content.options;
 
   return (
     <div className="form flex flex-col relative my-2 w-full">
@@ -27,17 +33,21 @@ export default function Select({ content }: elementType) {
         aria-invalid={errors[content.name] ? "true" : "false"}
         {...register(content.name)}
       >
-        {content.options?.map((item) => {
+        {optionsArray?.map((item: any) => {
           const option =
-            item.text === "Total"
-              ? `$${priceValue} (${item.text})`
-              : item.value && content.name === "pricePeriod"
-              ? `${inputValue}${item.text}`
-              : item.text;
+            item?.text === "Total"
+              ? `$${priceValue} (${item?.text})`
+              : item?.value && content.name === "pricePeriod"
+              ? `${inputValue}${item?.text}`
+              : item?.text;
 
+          const selectOption =
+            content.name === "country"
+              ? { value: item?.Iso2, text: item?.name }
+              : { value: item.value, text: option };
           return (
-            <option key={item.value} value={item.value}>
-              {option}
+            <option key={selectOption?.value} value={selectOption.value}>
+              {selectOption.text}
             </option>
           );
         })}
