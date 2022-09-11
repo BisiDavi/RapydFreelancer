70ff7fffdd3b1d651db2d87d86e605ae31d6964a
BisiDavi <oludavidconnect@gmail.com>
2022-09-11 01:59:36 +0100

    user message view updated

--- views/MessageView.tsx
+++ views/MessageView.tsx
@@ -13,6 +13,8 @@ export default function MessageView() {
     return readMessage.read ? "read" : "unread";
   }
 
+  const unreadMessages = messages.filter((item) => !item.read).length;
+
   function onClickHandler(id: string) {
     setShowMessage(!showMessage);
     let readMessage: any = messages.filter((item) => item.id === id)[0];
@@ -27,44 +29,58 @@ export default function MessageView() {
 
   return (
     <div>
-      <ul>
-        {messages.map((item) => {
-          const messageStatus = isMessageRead(item.id);
-          const statusStyle =
-            messageStatus === "read" ? "text-red-500" : "text-blue-500";
-          return (
-            <li key={item.id} className="bg-white w-full">
-              <div
-                className="title h-12 flex mx-auto items-center px-4 shadow w-full relative  cursor-pointer"
-                onClick={() => onClickHandler(item.id)}
-              >
-                <h4 className="text-xl font-medium">{item.title}...</h4>
-                <small className="text-right absolute text-red-500 right-4">
-                  <span className={`mx-1 ${statusStyle}`}>
-                    ({messageStatus})
-                  </span>
-                  click to read more
-                </small>
-              </div>
-              {showMessage && (
-                <div className="message text-lg p-4">
-                  {item.message.map((messageGroup: string[], index: number) => {
-                    return (
-                      <Fragment key={index}>
-                        {messageGroup.map((message) => (
-                          <p key={message} className="my-2">
-                            {message}
-                          </p>
-                        ))}
-                      </Fragment>
-                    );
-                  })}
+      <h4 className="text-center text-xl my-4 font-bold">View your Inbox</h4>
+      <h6 className="my-1 text-blue-500 font-bold">
+        Total Messages: {messages.length} message(s)
+      </h6>
+      <h6 className="my-1 mb-4 text-blue-500 font-bold">
+        unread messages: {unreadMessages} message(s)
+      </h6>
+
+      {messages.length > 0 ? (
+        <ul>
+          {messages.map((item) => {
+            const messageStatus = isMessageRead(item.id);
+            const statusStyle =
+              messageStatus === "read" ? "text-red-500" : "text-blue-500";
+            return (
+              <li key={item.id} className="bg-white w-full">
+                <div
+                  className="title h-12 flex mx-auto items-center px-4 shadow w-full relative  cursor-pointer"
+                  onClick={() => onClickHandler(item.id)}
+                >
+                  <h4 className="text-xl font-medium">{item.title}...</h4>
+                  <small className="text-right absolute text-red-500 right-4">
+                    <span className={`mx-1 ${statusStyle}`}>
+                      ({messageStatus})
+                    </span>
+                    click to read more
+                  </small>
                 </div>
-              )}
-            </li>
-          );
-        })}
-      </ul>
+                {showMessage && (
+                  <div className="message text-lg p-4">
+                    {item.message.map(
+                      (messageGroup: string[], index: number) => {
+                        return (
+                          <Fragment key={index}>
+                            {messageGroup.map((message) => (
+                              <p key={message} className="my-2">
+                                {message}
+                              </p>
+                            ))}
+                          </Fragment>
+                        );
+                      }
+                    )}
+                  </div>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p className="text-center text-xl font-bold">Oops no messages yet</p>
+      )}
     </div>
   );
 }
