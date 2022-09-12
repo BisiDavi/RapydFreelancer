982356f99a9ae1d35c729821dfc3bfaba0901ef4
BisiDavi <oludavidconnect@gmail.com>
2022-09-12 13:35:01 +0100

    profile form updated

--- components/form/schema/profileSchema.ts
+++ components/form/schema/profileSchema.ts
@@ -2,6 +2,7 @@ import * as yup from "yup";
 
 export const profileSchema = yup.object({
   address: yup.string().required("Address is required"),
+  phonenumber: yup.string().required("Phone number is required"),
   city: yup.string().required("City is required"),
   state: yup.string().required("State is required"),
   zip: yup.string().required("Enter Zip code"),
