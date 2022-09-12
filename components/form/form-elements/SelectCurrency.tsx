da93d3ebb3e7a0f386566cf7b8a0ec48c2285ad7
BisiDavi <oludavidconnect@gmail.com>
2022-09-12 18:58:35 +0100

    account form updated

---
+++ components/form/form-elements/SelectCurrency.tsx
@@ -0,0 +1,58 @@
+import { useFormContext } from "react-hook-form";
+
+import type { elementType } from "@/types/form-types";
+import countriesData from "@/json/countries.json";
+
+export default function Select({ content }: elementType) {
+  const {
+    register,
+    watch,
+    formState: { errors },
+  }: any = useFormContext();
+
+  const priceValue = watch("price") ? watch("price") : 0;
+
+  const inputValue = content.name === "pricePeriod" ? `$${priceValue} / ` : "";
+  const labelClassName =
+    content?.inputStyle === "big"
+      ? "font-bold my-1 text-lg"
+      : "font-medium my-1 text-base";
+
+  const countries: { name: string; Iso2: string | any }[] = countriesData.data;
+
+  const optionsArray = content.name === "country" ? countries : content.options;
+
+  return (
+    <div className="form flex flex-col relative my-2 w-full">
+      <label htmlFor={content.name} className={labelClassName}>
+        {content.label}
+      </label>
+      <select
+        id={content.name}
+        className="rounded-lg h-10 px-4 border"
+        aria-invalid={errors[content.name] ? "true" : "false"}
+        {...register(content.name)}
+      >
+        {optionsArray?.map((item: any) => {
+          const option =
+            item?.text === "Total"
+              ? `$${priceValue} (${item?.text})`
+              : item?.value && content.name === "pricePeriod"
+              ? `${inputValue}${item?.text}`
+              : item?.text;
+
+          const selectOption =
+            content.name === "country"
+              ? { value: item?.Iso2, text: item?.name }
+              : { value: item.value, text: option };
+          return (
+            <option key={selectOption?.value} value={selectOption.value}>
+              {selectOption.text}
+            </option>
+          );
+        })}
+      </select>
+      <p className="text-red-500 text-xs">{errors[content.name]?.message}</p>
+    </div>
+  );
+}
--- views/ProfileView.tsx
+++ views/ProfileView.tsx
@@ -4,6 +4,7 @@ import ProfileForm from "@/components/form/ProfileForm";
 import { useAppSelector } from "@/hooks/useRedux";
 import { getWallet } from "@/request/getRequest";
 import IssueVirtualAccountForm from "@/components/form/IssueVirtualAccountForm";
+import SpinnerRipple from "@/components/loader/SpinnerRipple";
 
 export default function ProfileView() {
   const { isProfileFormFilled } = useAppSelector((state) => state.form);
@@ -27,7 +28,26 @@ export default function ProfileView() {
         with us
       </p>
       <h2 className="text-xl">Account Details</h2>
-
+      {status === "error" ? (
+        "unable to fetch account details"
+      ) : status === "loading" ? (
+        <div className="view h-20 mx-auto flex justify-center items-center w-full">
+          <SpinnerRipple centerRipple />
+        </div>
+      ) : (
+        <div className="account-view flex items-center my-2 shadow py-4 px-6 rounded-full bg-white justify-between">
+          <p>Wallet ID: {data?.data.id}</p>
+          <p>
+            Status:{" "}
+            <span className="bg-green-500 px-2 text-sm  py-0.5 rounded-full ml-1 text-white">
+              {" "}
+              Active
+            </span>
+          </p>
+          <p>Category:{data.data.category ? data.data.category : "nill"}</p>
+          <p>Type:{data.data.type}</p>
+        </div>
+      )}
       <p className="font-semibol mt-4">
         Open an account with us to make you wallet active, fill the form below
       </p>
