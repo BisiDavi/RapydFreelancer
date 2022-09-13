dbc2850520b9b14d81e07f79763f79729aa8ca87
BisiDavi <oludavidconnect@gmail.com>
2022-09-13 18:19:24 +0100

    bid form added

---
+++ components/form/form-elements/BidForm.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable @next/next/no-img-element */
+import ContentEditable from "react-contenteditable";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+import Button from "@/components/UI/Button";
+import Media from "@/components/form/form-elements/Media";
+import useBidJob from "@/hooks/useBidJob";
+
+interface Props {
+  job: { id: string };
+}
+
+export default function BidForm({ job }: Props) {
+  const [previewMedia, setPreviewMedia] = useState([]);
+  const [html, setHtml] = useState(
+    "Get started with your <b>Proposal</b> here !"
+  );
+
+  const { createBid } = useBidJob();
+  function handleChange(e: any) {
+    setHtml(e.target.value);
+  }
+
+  const previewClassname =
+    previewMedia.length > 1 ? "grid grid-cols-2 gap-4" : "flex";
+
+  const words = html.length > 0 ? html.split(" ") : [];
+  const wordCount = words.length > 1 ? "words" : "word";
+
+  function createBidHandler() {
+    if (words.length > 50) {
+      createBid(html, job.id);
+    } else {
+      toast.error(
+        "Your Proposal is important and it must be at least 50 words"
+      );
+    }
+  }
+
+  return (
+    <>
+      <div className="proposal-view my-4 mb-8">
+        <ContentEditable
+          className="w-full border p-4 border-blue-500 rounded-xl h-400"
+          html={html}
+          onChange={handleChange}
+        />
+        <span className="font-bold">
+          {words.length} {wordCount}
+        </span>
+      </div>
+      <Media
+        big
+        previewMedia={previewMedia}
+        setPreviewMedia={setPreviewMedia}
+        content={{
+          name: "media",
+          placeholder: "Attach document (optional)",
+          label: "Upload a document (docs/pdf/picture) - (optional)",
+          type: "media",
+          elementType: "media",
+        }}
+      />
+      {previewMedia.length > 0 && (
+        <div className={`attached-document ${previewClassname}`}>
+          {previewMedia.map((item, index) => (
+            <img key={index} src={item} alt="preview attached documents" />
+          ))}
+        </div>
+      )}
+      <Button
+        text="Submit"
+        className="bg-green-600 text-white mt-8 w-24 h-10 mx-auto justify-center items-center flex hover:bg-green-400 font-bold"
+        onClick={createBidHandler}
+      />
+    </>
+  );
+}
--- pages/bid/[...slug].tsx
+++ pages/bid/[...slug].tsx
@@ -1,16 +1,11 @@
-/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/exhaustive-deps */
-import ContentEditable from "react-contenteditable";
-import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 import DefaultLayout from "@/layout/DefaultLayout";
 import { DBClient } from "@/db/DBConnection";
 import { getDataDB } from "@/db";
 import Breadcrumb from "@/components/BreadCrumb";
 import JobBanner from "@/components/banners/JobBanner";
-import Button from "@/components/UI/Button";
-import Media from "@/components/form/form-elements/Media";
 import JobDescription from "@/views/JobDescription";
 import useAuth from "@/hooks/useAuth";
 import { useAppDispatch } from "@/redux/store";
@@ -19,7 +14,7 @@ import ErrorView from "@/views/ErrorView";
 
 import type { GetServerSidePropsContext } from "next";
 import type { jobType } from "@/types";
-import useBidJob from "@/hooks/useBidJob";
+import BidForm from "@/components/form/form-elements/BidForm";
 
 interface Props {
   job: string;
@@ -30,39 +25,12 @@ export default function BiddingPage({ job }: Props) {
   const { authDetails } = useAuth();
   const auth = authDetails();
   const dispatch = useAppDispatch();
-  const [html, setHtml] = useState(
-    "Get started with your <b>Proposal</b> here !"
-  );
-  const [previewMedia, setPreviewMedia] = useState([]);
-
-  const { createBid } = useBidJob();
-  function handleChange(e: any) {
-    setHtml(e.target.value);
-  }
-
-  console.log("html", html);
-
-  const words = html.length > 0 ? html.split(" ") : [];
-  const wordCount = words.length > 1 ? "words" : "word";
-
-  function createBidHandler() {
-    if (words.length > 50) {
-      createBid(html, parsedJob.id);
-    } else {
-      toast.error(
-        "Your Proposal is important and it must be at least 50 words"
-      );
-    }
-  }
 
   useEffect(() => {
     if (!auth) {
       dispatch(updateModal("auth-modal"));
     }
   }, [auth]);
-
-  const previewClassname =
-    previewMedia.length > 1 ? "grid grid-cols-2 gap-4" : "flex";
 
   return (
     <DefaultLayout className="bg-gray-200 pb-4">
@@ -73,42 +41,7 @@ export default function BiddingPage({ job }: Props) {
             <Breadcrumb title={parsedJob.title} />
             <div className="content bg-white px-6 py-2 pb-6 rounded my-4 mb-8">
               <JobDescription job={parsedJob} />
-              <div className="proposal-view my-4 mb-8">
-                <ContentEditable
-                  className="w-full border p-4 border-blue-500 rounded-xl h-400"
-                  html={html}
-                  onChange={handleChange}
-                />
-                <span className="font-bold">
-                  {words.length} {wordCount}
-                </span>
-              </div>
-              <Media
-                big
-                previewMedia={previewMedia}
-                setPreviewMedia={setPreviewMedia}
-                content={{
-                  name: "media",
-                  placeholder: "Attach document (optional)",
-                  label: "Upload a document (docs/pdf/picture) - (optional)",
-                  type: "media",
-                  elementType: "media",
-                }}
-              />
-              <div className={`attached-document ${previewClassname}`}>
-                {previewMedia.map((item, index) => (
-                  <img
-                    key={index}
-                    src={item}
-                    alt="preview attached documents"
-                  />
-                ))}
-              </div>
-              <Button
-                text="Submit"
-                className="bg-green-600 text-white mt-8 w-24 h-10 mx-auto justify-center items-center flex hover:bg-green-400 font-bold"
-                onClick={createBidHandler}
-              />
+              <BidForm job={parsedJob} />
             </div>
           </section>
         </>
