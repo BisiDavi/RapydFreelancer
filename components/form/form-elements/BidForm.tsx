248143499a9dcfdda8732859bd82d6121cf481b1
BisiDavi <oludavidconnect@gmail.com>
2022-09-14 04:43:02 +0100

    form updated

--- components/form/form-elements/BidForm.tsx
+++ components/form/form-elements/BidForm.tsx
@@ -2,6 +2,7 @@
 import ContentEditable from "react-contenteditable";
 import { useState } from "react";
 import { toast } from "react-toastify";
+import Link from "next/link";
 
 import Button from "@/components/UI/Button";
 import Media from "@/components/form/form-elements/Media";
@@ -19,7 +20,7 @@ export default function BidForm({ job }: Props) {
     "Get started with your <b>Proposal</b> here !"
   );
 
-  const { mutate, isLoading } = useBidMutation();
+  const { mutate, isLoading, isSuccess } = useBidMutation();
   const { authDetails } = useAuth();
   const auth: any = authDetails();
 
@@ -57,43 +58,58 @@ export default function BidForm({ job }: Props) {
 
   return (
     <>
-      <div className="proposal-view my-4 mb-8">
-        <ContentEditable
-          className="w-full border p-4 border-blue-500 rounded-xl h-400"
-          html={html}
-          onChange={handleChange}
-        />
-        <span className="font-bold">
-          {words.length} {wordCount}
-        </span>
-      </div>
-      {words.length >= 50 && (
-        <Media
-          big
-          previewMedia={previewMedia}
-          setPreviewMedia={setPreviewMedia}
-          content={{
-            name: "media",
-            placeholder: "Attach document (optional)",
-            label: "Upload a document (docs/pdf/picture) - (optional)",
-            type: "media",
-            elementType: "media",
-          }}
-        />
-      )}
-      {previewMedia.length > 0 && (
-        <div className={`attached-document ${previewClassname}`}>
-          {previewMedia.map((item, index) => (
-            <img key={index} src={item} alt="preview attached documents" />
-          ))}
+      {isSuccess ? (
+        <div className="success flex items-center justify-between mx-auto my-6 p-3">
+          <img src="/checkmark.gif" alt="bid successful" />
+          <p className="font-bold text-center">
+            Hurray, You&#39;ve successfully submitted your bid, you can bid on
+            more jobs or visit your profile{" "}
+            <Link href="/user/profile">
+              <a className="mx-1 text-red-500 hover:text-400">Profile</a>
+            </Link>
+          </p>
         </div>
+      ) : (
+        <>
+          <div className="proposal-view my-4 mb-8">
+            <ContentEditable
+              className="w-full border p-4 border-blue-500 rounded-xl h-400"
+              html={html}
+              onChange={handleChange}
+            />
+            <span className="font-bold">
+              {words.length} {wordCount}
+            </span>
+          </div>
+          {words.length >= 50 && (
+            <Media
+              big
+              previewMedia={previewMedia}
+              setPreviewMedia={setPreviewMedia}
+              content={{
+                name: "media",
+                placeholder: "Attach document (optional)",
+                label: "Upload a document (docs/pdf/picture) - (optional)",
+                type: "media",
+                elementType: "media",
+              }}
+            />
+          )}
+          {previewMedia.length > 0 && (
+            <div className={`attached-document ${previewClassname}`}>
+              {previewMedia.map((item, index) => (
+                <img key={index} src={item} alt="preview attached documents" />
+              ))}
+            </div>
+          )}
+          <Button
+            text="Submit"
+            className="bg-green-600 text-white mt-8 w-24 h-10 mx-auto justify-center items-center flex hover:bg-green-400 font-bold"
+            onClick={createBidHandler}
+            loading={isLoading}
+          />
+        </>
       )}
-      <Button
-        text="Submit"
-        className="bg-green-600 text-white mt-8 w-24 h-10 mx-auto justify-center items-center flex hover:bg-green-400 font-bold"
-        onClick={createBidHandler}
-        loading={isLoading}
-      />
     </>
   );
 }
