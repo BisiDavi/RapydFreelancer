9a5dead7443e65cf1f32913f2712e0efdc122c94
BisiDavi <oludavidconnect@gmail.com>
2022-09-17 05:22:58 +0100

    pages updated

--- pages/payment/connect/error.tsx
+++ pages/payment/connect/error.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
+import Button from "@/components/UI/Button";
 import DefaultLayout from "@/layout/DefaultLayout";
-import ProfileSidebar from "@/components/sidebar/ProfileSidebar";
 
 export default function ConnectPaymentError() {
   return (
     <DefaultLayout title="Your Profile">
       <section className="container flex items-start mx-auto my-10">
-        <ProfileSidebar />
-        <div className="content w-4/5 mx-auto flex flex-col justify-center p-6 item-start ml-4 rounded  bg-gray-100">
+        <div className="content w-full mx-auto flex flex-col justify-center p-6  ml-4 rounded  bg-gray-100">
           <img
             src="/error-icon.webp"
             alt="successful"
@@ -16,6 +15,18 @@ export default function ConnectPaymentError() {
           <h4 className="font-bold text-center my-4">
             Error purchasing Connect
           </h4>
+          <div className="button-group w-1/3 mx-auto justify-between flex items-center">
+            <Button
+              text="Retry"
+              className="py-1 bg-green-500 hover:opacity-80 text-white px-6 rounded-md"
+              href="/user/account"
+            />
+            <Button
+              text="Home"
+              className="py-1 bg-red-500 text-white px-6 rounded-md hover:opacity-80"
+              href="/"
+            />
+          </div>
         </div>
       </section>
     </DefaultLayout>
