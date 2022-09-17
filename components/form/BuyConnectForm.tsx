011e8a9be9da7de7f1cd4e7896bcc7d0d5579d2d
BisiDavi <oludavidconnect@gmail.com>
2022-09-17 03:44:59 +0100

    payment method updated

--- components/form/BuyConnectForm.tsx
+++ components/form/BuyConnectForm.tsx
@@ -9,6 +9,17 @@ import SelectCurrency from "@/components/form/form-elements/SelectCurrency";
 import usePaymentMutation, {
   getCurrencyDailyRate,
 } from "@/hooks/usePaymentMutation";
+import { SpinnerLoader } from "@/components/loader/SpinnerRipple";
+
+function getConnectQuantity(connectPrice: string) {
+  if (connectPrice === "20") {
+    return 10;
+  } else if (connectPrice === "50") {
+    return 50;
+  } else if (connectPrice === "90") {
+    return 100;
+  }
+}
 
 export default function BuyConnectForm() {
   const methods = useForm({
@@ -23,7 +34,7 @@ export default function BuyConnectForm() {
   const formValues = methods.watch();
   const currencyType = methods.watch("currencyType");
   const currency = methods.watch("currency");
-  const connectQuantity = methods.watch("connectQuantity");
+  const connectPrice = methods.watch("connectQuantity");
   const { useGetCurrencyRate } = usePaymentMutation();
   const { mutate, data, status, isLoading } = useGetCurrencyRate();
 
@@ -33,6 +44,24 @@ export default function BuyConnectForm() {
   function fetchExchangeRate() {
     mutate(currency);
   }
+
+  console.log("data-mutation", data?.data);
+  console.log("status-mutation", status);
+
+  const exchangeRate =
+    status === "success" ? Number(connectPrice) * data?.data.rate : "";
+
+  const payAmount =
+    currencyType === "USD"
+      ? `$${connectPrice}`
+      : `${currency} ${exchangeRate}`;
+
+  const paymentCheck =
+    currencyType === "USD" && connectPrice
+      ? true
+      : status === "success"
+      ? true
+      : false;
 
   return (
     <FormProvider {...methods}>
@@ -46,7 +75,7 @@ export default function BuyConnectForm() {
           )}
         </div>
 
-        {currency && (
+        {currencyType !== "USD" && currency && (
           <Button
             text={`Fetch currency exchange rate of ${currency}/USD`}
             className="bg-green-500 mx-auto flex my-2 mt-4 px-4 py-2 text-white rounded-md"
@@ -54,12 +83,28 @@ export default function BuyConnectForm() {
             onClick={fetchExchangeRate}
           />
         )}
-        {/* {currency && (
+        {status === "error" ? (
+          "unable to fetch exchange rate"
+        ) : status === "loading" ? (
+          <SpinnerLoader loadingText="fetching exchange rate" />
+        ) : (
+          status === "success" && (
+            <div className="exchange-rate my-2">
+              <h4 className="font-bold">
+                1 USD = {data?.data.rate} {currency}
+              </h4>
+              <h4 className="font-bold">
+                {connectPrice} USD = {exchangeRate} {currency}
+              </h4>
+            </div>
+          )
+        )}
+        {paymentCheck && (
           <Button
-            text={`Proceed to Pay $${connectQuantity}`}
+            text={`Proceed to Pay ${payAmount}`}
             className="bg-green-500 mx-auto flex my-2 mt-4 px-4 py-2 text-white rounded-md"
           />
-        )} */}
+        )}
       </form>
     </FormProvider>
   );
