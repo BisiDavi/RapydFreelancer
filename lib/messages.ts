7d5b55ae788a6879d8f95f26a8d59b11aab9922f
BisiDavi <oludavidconnect@gmail.com>
2022-09-19 12:47:13 +0100

    messages updated

--- lib/messages.ts
+++ lib/messages.ts
@@ -19,7 +19,7 @@ export function bidMessageToRecruiter(data: bidMessageType) {
     title: `Hello, ${data.freelancer.displayName} just bidded on your job listing - ${data.title}`,
     message: [
       [
-        `🎉 Congrats, your Job listing is getting attention, Get to read ${data.freelancer.displayName}'s proposal for your job listing`,
+        `🎉 Congrats,  your Job listing is getting attention, Get to read ${data.freelancer.displayName}'s proposal for your job listing`,
         `You can always chat up ${data.freelancer.displayName}, wishing you a successful business transaction.`,
       ],
     ],
@@ -34,7 +34,7 @@ export function bidMessageToFreelancer(data: bidMessageType) {
     title: `Congrats ${data.freelancer.displayName}, you just bidded on this job listing - ${data.title}`,
     message: [
       [
-        `🎉 Congrats, we care about each proposals you send and we are ready to help you win the job, by always notifying you as at when due.`,
+        `🎉 Congrats, on your bid on ${data.title}, we care about each proposals you send and we are ready to help you win the job, by always notifying you as at when due.`,
         `Look forward to receiving a notification if hired for the job`,
       ],
     ],
