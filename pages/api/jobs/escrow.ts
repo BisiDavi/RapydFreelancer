c0e5a4b57e0cb405457afc218d879970178e1435
BisiDavi <oludavidconnect@gmail.com>
2022-09-19 19:28:39 +0100

    pages updated

--- pages/api/jobs/escrow.ts
+++ pages/api/jobs/escrow.ts
@@ -19,19 +19,13 @@ export default async function handler(
           { email: query.email },
           { $push: { hires: data } }
         );
-        return await updateDataDB(
+        const response = await updateDataDB(
           dbClient,
           "jobs",
           { id: query.id },
           { $push: { hired: data }, $set: { paid: true, active: false } }
-        )
-          .then(() => {
-            return res.status(200).send("escrow payment made");
-          })
-          .catch((error) => {
-            console.log("error", error);
-            return res.status(400).send("job not created");
-          });
+        );
+        return res.status(200).send(response.data);
       } catch (err) {
         console.log("post-error", err);
         return res.status(400).send(err);
